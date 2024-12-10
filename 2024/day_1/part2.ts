c75b14fcc5c83043e74fa2654984361ff547b23e
Aaron Dill <aaronsacks2006@gmail.com>
2024-12-10 10:19:50 -0600

    refactor(2024): day_1 ts linting

--- 2024/day_1/part2.ts
+++ 2024/day_1/part2.ts
@@ -8,13 +8,14 @@ const [first, second] = lines
   .map(line => line.split(/\s+/).map(Number))
   .reduce((acc: number[][], line) => {
     // Transpose the arrays
-    for (let i = 0; i < line.length; i++) (acc[i] ??= []).push(line[i]);
+    for (let i = 0; i < line.length; i++) (acc[i] ??= []).push(line[i]!);
     return acc;
   }, []);
+if (!first || !second) throw new Error("No input");
 first.sort((a, b) => a - b);
 second.sort((a, b) => a - b);
 
-const counts = {};
+const counts: Record<number, number> = {};
 for (const n of second) counts[n] = (counts[n] ?? 0) + 1;
 
 const similarity = first.map(n => n * (counts[n] ?? 0)).reduce((a, b) => a + b);

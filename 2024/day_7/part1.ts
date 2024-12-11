b3129499a89a9fac3a73c32f4975ac1b579aa010
Aaron Dill <aaronsacks2006@gmail.com>
2024-12-11 08:08:55 -0600

    feat(2024): day_7 ts

---
+++ 2024/day_7/part1.ts
@@ -0,0 +1,21 @@
+// https://adventofcode.com/2024/day/1
+
+import { readInput } from "../../utils/utils";
+
+const input = await readInput();
+const lines = input.split("\n").filter(Boolean);
+const nums = lines.map(line => line.split(/:?\s+/).map(Number));
+
+function evaluate(nums: number[], target: number, current = 0): boolean {
+  const [first, ...rest] = nums;
+  if (first == undefined) return target === current;
+  if (evaluate(rest, target, current + first)) return true;
+  if (evaluate(rest, target, current * first)) return true;
+  return false;
+}
+
+const result = nums
+  .filter(n => evaluate(n.slice(1), n[0]!))
+  .map(n => n[0]!)
+  .reduce((a, b) => a + b);
+console.log(result);

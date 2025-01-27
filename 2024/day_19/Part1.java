35634b62dd7fe8ec1896d9e62b8952e0afd1b50e
Aaron Dill <aaronsacks2006@gmail.com>
2025-01-27 13:44:57 -0600

    feat(2024): day_19 java

---
+++ 2024/day_19/Part1.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Part1 {
+  private static Scanner readInput(String[] args) throws FileNotFoundException {
+    if (args.length == 0) return new Scanner(System.in);
+    return new Scanner(new File(args[0]));
+  }
+
+  public static void main(String[] args) throws FileNotFoundException {
+    List<String> availablePatterns = new ArrayList<>();
+    List<String> desiredPatterns = new ArrayList<>();
+
+    try (Scanner scan = readInput(args)) {
+      availablePatterns.addAll(Arrays.asList(scan.nextLine().split(", "))); // r, wr, b, g, bwu, rb, gb, br
+      scan.nextLine(); // empty line
+      while (scan.hasNextLine()) {
+        String pattern = scan.nextLine(); // brwrr
+        desiredPatterns.add(pattern);
+      }
+    }
+
+    System.out.println(desiredPatterns.stream().parallel().filter(p -> isPossible(p, availablePatterns))
+        .peek(p -> System.out.println(p)).count());
+  }
+
+  private static boolean isPossible(String design, List<String> patterns) {
+    var lcm = new HashMap<Integer, Long>(Map.of(0, 1l));
+    for (int i = 0; i < design.length(); i++) {
+      for (String pattern : patterns) {
+        int end = i + pattern.length();
+        if (end > design.length()) continue;
+        if (!design.startsWith(pattern, i)) continue;
+        lcm.merge(end, lcm.getOrDefault(i, 0l), Long::sum);
+      }
+    }
+    return lcm.get(design.length()) > 0;
+  }
+}
